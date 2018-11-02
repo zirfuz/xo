1ece4e4b1bba8b46cffc7209dc66a955123cc2ec
AO7 <asdf7@asdf.asdf>
2018-11-02 09:49:45 +0300

    z

--- tictactoe.py
+++ tictactoe.py
@@ -6,7 +6,8 @@ import winsound
 def beep(freq, dur):
     winsound.Beep(freq, dur)
 
-G_SIZE = 12
+G_SIZE = 20
+G_BUTTON_SIZE = 1
 
 class TicTacToe:
     def __init__(self, size):
@@ -16,23 +17,40 @@ class TicTacToe:
         self.__root.resizable(0,0)
         self.__root.wm_title('Tic-Tac-Toe')
         img_arg = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
-        self.__emptyImg = tk.PhotoImage(img_arg)
+        self.__empty_img = tk.PhotoImage(img_arg)
         self.__ttt = TicTacToeCore(size)
         self.__cong = False
+
         self.__buttons = [[None] * size for _ in range(size)]
         for i in range(size):
             for j in range(size):
-                self.__buttons[i][j] = tk.Button(self.__frame, width=2, height=1, disabledforeground="blue", font=('', 10, 'bold'))
+                # self.__buttons[i][j] = tk.Button(self.__frame, image=self.__empty_img, height=16, width=16, disabledforeground="blue", font=('', 10, 'bold'))
+                self.__buttons[i][j] = tk.Button(self.__frame, height=1, width=2, disabledforeground="blue", font=('', 10, 'bold'))
                 self.__buttons[i][j].grid(row=i, column=j)
                 self.__buttons[i][j].bind('<Button>', self.__clicked)
-        self.__button1 = tk.Button(self.__frame, text="Player", fg="blue")
+
+        self.__button1 = tk.Button(self.__frame, text="Player", fg="blue", activeforeground='blue')
         self.__button1.grid(row=size, column=0, columnspan=2)
-        self.__button2 = tk.Button(self.__frame, text="Player", fg="red")
+        self.__button1.bind('<Button>', self.__change_player)
+
+        self.__button2 = tk.Button(self.__frame, text="Player", fg="red", activeforeground='red')
         self.__button2.grid(row=size, column=size-2, columnspan=2)
+        self.__button2.bind('<Button>', self.__change_player)
+
         self.__new_res = tk.Button(self.__frame, text="New", command=self.__reset)
         self.__new_res.grid(row=size, column=(size-1)//2, columnspan=2)
+
+
     def run(self):
         self.__root.mainloop()
+
+
+    def __change_player(self, event):
+        but = event.widget
+        but['text'] = 'Player' if but['text'] == 'AI' else 'AI'
+        beep(150, 75)
+
+
     def __clicked(self, event):
         but = event.widget
         grid_info = but.grid_info()
@@ -53,12 +71,16 @@ class TicTacToe:
             for cell in win_cells:
                 self.__buttons[cell[0]][cell[1]]['background'] = 'orange'
             beep(400, 500)
+
+
     def __set_buttons_state(self, val):
         for i in range(self.__ttt.size):
             for j in range(self.__ttt.size):
                 self.__buttons[i][j]['state'] = 'normal' if val else 'disabled'
         #state = 'normal' if val else 'disabled'
         #self.__for_each(self, lambda self, but, state: but['state']=state)
+
+
     def __reset(self):
         self.__set_buttons_state(True)
         for i in range(self.__ttt.size):
